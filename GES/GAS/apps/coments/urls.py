0216b47caa86d0940e680a7915285c75f307befb
/fcg <stasyanrus610@gmail.com>
2023-11-30 21:54:51 +0300

    Убрал лишние строки url web/commetns/comments/1

--- GES/GAS/apps/coments/urls.py
+++ GES/GAS/apps/coments/urls.py
@@ -9,6 +9,6 @@ from django.conf.urls.static import static
 app_name = 'comments'
 
 urlpatterns = [
-    path('comments/<int:idOfPage>/', views.index, name='index'),
+    path('<int:idOfPage>/', views.index, name='index'),
     path('leave_comment/<int:idOfPage>/', views.leave_comment, name='leave_comment'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
