96510a61f09664bcb53b7b09cffe6992b8dc1736
/fcg <stasyanrus610@gmail.com>
2024-01-08 21:21:34 +0300

    About US

--- GES/GAS/settings.py
+++ GES/GAS/settings.py
@@ -140,3 +140,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
