d670c9743665ccebf86b6f10d73bd7194a1f89ce
/fcg <stasyanrus610@gmail.com>
2024-05-19 18:35:27 +0300

    POSTGRESQL database

--- GES/GAS/settings.py
+++ GES/GAS/settings.py
@@ -81,11 +81,21 @@ WSGI_APPLICATION = 'GAS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES= {
+        'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'HOST': os.environ.get('DB_HOST'),
+                'NAME': os.environ.get('DB_NAME'),
+                'USER': os.environ.get('DB_USER'),
+                'PASSWORD': os.environ.get('DB_PASS'),
+            }
 }
 
 # Password validation
