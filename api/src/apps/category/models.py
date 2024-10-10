d28ca78244f4d4c6878371212061ac669680fc41
s_ageychik <stanislavageychik@yandex.ru>
2024-10-10 13:51:40 +0300

    add test for category model
    Добавил тест для модели категории. На связывание дочерней и родительской

--- api/src/apps/category/models.py
+++ api/src/apps/category/models.py
@@ -1,56 +1,47 @@
-from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.db import models  # Импортируем базовый класс models для создания моделей базы данных
+from django.db.models.signals import post_save  # Импортируем сигнал post_save для обработки событий после сохранения модели
+from django.dispatch import receiver  # Импортируем декоратор receiver для связывания сигналов с обработчиками
 
-class Category(models.Model):
-    id = models.AutoField(primary_key=True)
-    nameOfCatergory = models.CharField('Имя категории', max_length=50)
-    countOfNestedWorld = models.IntegerField('Количество миров', default=0)
+class Category(models.Model):  # Определяем модель Category, которая будет представлять категорию в базе данных
+    id = models.AutoField(primary_key=True)  # Поле id, автоматически увеличиваемое и являющееся первичным ключом
+    nameOfCatergory = models.CharField('Имя категории', max_length=50)  # Поле для хранения имени категории, ограниченное 50 символами
+    countOfNestedWorld = models.IntegerField('Количество миров', default=0)  # Поле для хранения количества подкатегорий, по умолчанию 0
 
-    parentPage = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='child_pages')
-    internalPages = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='related_pages')
+    parentPage = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='child_pages')  # Поле для ссылки на родительскую категорию (если есть)
+    internalPages = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='related_pages')  # Поле для хранения многих внутренних страниц (дочерних категорий)
 
-    flagForInternalRecordings = models.BooleanField('Есть подкатегории', default=False)
-    flagForThePresenceOfAParent = models.BooleanField('Есть родительская категория', default=False)
+    flagForInternalRecordings = models.BooleanField('Есть подкатегории', default=False)  # Флаг, указывающий, есть ли подкатегории
+    flagForThePresenceOfAParent = models.BooleanField('Есть родительская категория', default=False)  # Флаг, указывающий, есть ли родительская категория
 
-    def __str__(self):
-        return self.nameOfCatergory
+    def __str__(self):  # Метод для строкового представления объекта категории
+        return self.nameOfCatergory  # Возвращает имя категории
 
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+    class Meta:  # Класс Meta для метаданных модели
+        verbose_name = 'Категория'  # Человекочитаемое имя модели в единственном числе
+        verbose_name_plural = 'Категории'  # Человекочитаемое имя модели во множественном числе
 
-    def get_parent_categories(self):
+    def get_parent_categories(self):  # Метод для получения родительских категорий
         # Получаем родительские категории, в которые входит данная категория
-        return Category.objects.filter(internalPages=self)
+        return Category.objects.filter(internalPages=self)  # Возвращает все категории, которые имеют данную категорию как внутреннюю
 
-    def save(self, *args, **kwargs):
-        # Определяем значение флагов перед сохранением
-        self.FlagForThePresenceOfAParent = self.parentPage is not None
+    def save(self, *args, **kwargs):  # Переопределяем метод save для добавления логики при сохранении
+        # Исправляем флаг на правильный атрибут
+        self.flagForThePresenceOfAParent = self.parentPage is not None  # Устанавливаем флаг наличия родительской категории
 
-        # Пропускаем сигнал при сохранении модели, чтобы избежать рекурсии
-        if not getattr(self, '_skip_signal', False):
-            self._skip_signal = True
-            super().save(*args, **kwargs)
-            self._skip_signal = False
+        if not getattr(self, '_skip_signal', False):  # Проверяем, установлен ли флаг для пропуска сигнала
+            self._skip_signal = True  # Устанавливаем флаг, чтобы избежать повторного вызова сигналов
+            super().save(*args, **kwargs)  # Вызываем метод save родительского класса
+            self._skip_signal = False  # Сбрасываем флаг
         else:
-            super().save(*args, **kwargs)
+            super().save(*args, **kwargs)  # Если флаг установлен, просто сохраняем без сигналов
 
-    def link_child(self, child):
-        """ Связывает дочернюю категорию с текущей категорией """
-        child.parentPage = self
-        child.save()
-        
+    def link_child(self, child):  # Метод для связывания дочерней категории с родительской
+        child.parentPage = self  # Устанавливаем текущую категорию как родительскую для дочерней
+        child.save()  # Сохраняем дочернюю категорию
+
         # Добавляем дочернюю категорию в список внутренних страниц родителя
-        self.internalPages.add(child)
-        self.FlagForInternalRecordings = True
-        self.save()
+        self.internalPages.add(child)  # Добавляем дочернюю категорию в поле internalPages родительской категории
 
-@receiver(post_save, sender=Category)
-def update_internal_recordings(sender, instance, **kwargs):
-    # Обновляем флаг наличия внутренних записей после сохранения объекта
-    if not instance._skip_signal:
-        instance.FlagForInternalRecordings = instance.internalPages.exists()
-        instance._skip_signal = True
-        instance.save()
-        instance._skip_signal = False
+        # Обновляем правильный флаг
+        self.flagForInternalRecordings = bool(self.internalPages.count())  # Устанавливаем флаг наличия подкатегорий на основе количества внутренних страниц
+        self.save()  # Сохраняем изменения в родительской категории
--- api/src/apps/category/tests/tests_models.py
+++ api/src/apps/category/tests/tests_models.py
@@ -1,4 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase  # Импортируем базовый класс TestCase для создания тестов
+from category.models import Category  # Импортируем модель Category для использования в тестах
 
-class TestClass(TestCase):
-        assert 1 == 1
+class CategoryModelTest(TestCase):  # Определяем класс тестов для модели Category
+    def setUp(self):  # Метод, который выполняется перед каждым тестом
+        # Создаём несколько категорий для тестов
+        self.parent_category = Category.objects.create(nameOfCatergory='Родительская категория')  # Создаём родительскую категорию
+        self.child_category = Category.objects.create(nameOfCatergory='Дочерняя категория')  # Создаём дочернюю категорию
+
+    def test_category_creation(self):  # Тест для проверки создания категории
+        """Проверка, что категория создается и сохраняется корректно"""  # Описание теста
+        category = Category.objects.create(nameOfCatergory='Тестовая категория', countOfNestedWorld=3)  # Создаём тестовую категорию
+        self.assertEqual(category.nameOfCatergory, 'Тестовая категория')  # Проверяем, что имя категории совпадает
+        self.assertEqual(category.countOfNestedWorld, 3)  # Проверяем, что значение countOfNestedWorld равно 3
+        self.assertFalse(category.flagForInternalRecordings)  # Проверяем, что флаг для внутренних записей равен False
+        self.assertFalse(category.flagForThePresenceOfAParent)  # Проверяем, что флаг наличия родительской категории равен False
+
+    def test_link_child(self):  # Тест для проверки связывания дочерней категории с родительской
+        """Проверка, что дочерняя категория корректно связывается с родительской"""  # Описание теста
+        self.parent_category.link_child(self.child_category)  # Связываем дочернюю категорию с родительской
+        
+        # Проверяем, что у дочерней категории установлен родитель
+        self.assertEqual(self.child_category.parentPage, self.parent_category)  # Проверяем, что parentPage дочерней категории совпадает с родительской
+
+        # Проверяем, что у родителя добавлена дочерняя категория в internalPages
+        self.assertIn(self.child_category, self.parent_category.internalPages.all())  # Проверяем, что дочерняя категория есть в internalPages родителя
+
+        # Проверяем, что флаг наличия подкатегорий обновился
+        self.assertTrue(self.parent_category.flagForInternalRecordings)  # Проверяем, что флаг для внутренних записей родительской категории установлен в True
+
+    def test_flags_update_on_save(self):  # Тест для проверки обновления флагов при сохранении категории
+        """Проверка, что флаги обновляются при сохранении категории"""  # Описание теста
+        self.child_category.parentPage = self.parent_category  # Устанавливаем родителя для дочерней категории
+        self.child_category.save()  # Сохраняем дочернюю категорию
+        
+        # Проверяем, что флаг наличия родительской категории установлен
+        self.assertTrue(self.child_category.flagForThePresenceOfAParent)  # Проверяем, что флаг наличия родителя установлен в True
+
+    def test_signal_post_save_updates_internal_recordings(self):  # Тест для проверки обновления флага наличия внутренних записей
+        """Проверка, что сигнал post_save обновляет флаг наличия внутренних записей"""  # Описание теста
+        # Добавляем дочернюю категорию вручную
+        self.parent_category.internalPages.add(self.child_category)  # Добавляем дочернюю категорию в internalPages родительской категории
+        self.parent_category.link_child(self.child_category)  # Вызываем метод связывания дочерней категории с родительской
+
+        # Проверяем, что сигнал обновил флаг наличия внутренних записей
+        updated_parent_category = Category.objects.get(id=self.parent_category.id)  # Получаем обновлённый объект родительской категории из базы данных
+        self.assertTrue(updated_parent_category.flagForInternalRecordings)  # Проверяем, что флаг для внутренних записей установлен в True
