703951f570614ec2355b62279bdf3273a277758e
s_ageychik <stanislavageychik@yandex.ru>
2024-10-31 15:15:49 +0300

    edit notify bot

--- notify/send_message_pipline.py
+++ notify/send_message_pipline.py
@@ -5,10 +5,16 @@ def send_message(token, chat_id, message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         'chat_id': chat_id,
-        'text': message
+        'text': message,
+        'parse_mode': 'HTML'  # Если хотите использовать HTML для форматирования
     }
     response = requests.post(url, data=payload)
-    return response.json()
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -17,5 +23,10 @@ if __name__ == "__main__":
 
     TOKEN = sys.argv[1]
     CHAT_ID = sys.argv[2]
-    COMMIT_MESSAGE = sys.argv[3]
-    send_message(TOKEN, CHAT_ID, COMMIT_MESSAGE)
+    COMMIT_MESSAGE = "🤖👨‍💻\nNew update: " + sys.argv[3]
+    
+    result = send_message(TOKEN, CHAT_ID, COMMIT_MESSAGE)
+    if result and result.get("ok"):
+        print("Message sent successfully!")
+    else:
+        print("Failed to send message.")
