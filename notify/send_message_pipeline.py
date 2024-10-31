0f3b15b375cb3fdd6fb551c2038a8b1689764cee
s_ageychik <stanislavageychik@yandex.ru>
2024-10-31 15:45:36 +0300

    another try...
    comment

--- notify/send_message_pipeline.py
+++ notify/send_message_pipeline.py
@@ -1,13 +1,19 @@
 import requests
 import sys
 
-def send_message(token, chat_id, message):
+def send_message(token, chat_id, commit_message, comments):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
+    # Формируем сообщение
+    message = f"🤖👨‍💻\nNew update: {commit_message}"
+    if comments:
+        message += f"\nComments: {comments}"
+
     payload = {
         'chat_id': chat_id,
         'text': message,
         'parse_mode': 'HTML'  # Если хотите использовать HTML для форматирования
     }
+    
     response = requests.post(url, data=payload)
     
     if response.status_code == 200:
@@ -17,15 +23,16 @@ def send_message(token, chat_id, message):
         return None
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python send_message.py <TOKEN> <CHAT_ID> <COMMIT_MESSAGE>")
+    if len(sys.argv) != 5:
+        print("Usage: python send_message.py <TOKEN> <CHAT_ID> <COMMIT_MESSAGE> <COMMENTS>")
         sys.exit(1)
 
     TOKEN = sys.argv[1]
     CHAT_ID = sys.argv[2]
-    COMMIT_MESSAGE = "🤖👨‍💻\nNew update: " + sys.argv[3]
+    COMMIT_MESSAGE = sys.argv[3]
+    COMMENTS = sys.argv[4]
     
-    result = send_message(TOKEN, CHAT_ID, COMMIT_MESSAGE)
+    result = send_message(TOKEN, CHAT_ID, COMMIT_MESSAGE, COMMENTS)
     if result and result.get("ok"):
         print("Message sent successfully!")
     else:
